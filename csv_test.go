0f50635dcbab236280c0a8eb5c55acaa2aca4e68
Corey Prophitt <git@prophitt.me>
2022-12-21 13:08:09 -0800

    Converted tests to use the file wrapper

--- csv_test.go
+++ csv_test.go
@@ -1,26 +1,13 @@
 package csv
 
 import (
-	"os"
 	"testing"
 )
 
 func TestProcessingPlainGzippedCSV(t *testing.T) {
-	f, err := os.Open("testfiles/people.csv.gz")
-
-	if err != nil {
-		t.Fatalf("test failed to open file; %s", err)
-	}
-
-	defer (func() {
-		if err := f.Close(); err != nil {
-			t.Fatalf("test failed to close file; %s", err)
-		}
-	})()
-
 	config := Config{Header: true, Gzipped: true}
 
-	processed, err := ForEach(f, config, func(*Row) error {
+	processed, err := ForEachFile("testfiles/people.csv.gz", config, func(*Row) error {
 		return nil /* NOP */
 	})
 
@@ -34,21 +21,9 @@ func TestProcessingPlainGzippedCSV(t *testing.T) {
 }
 
 func TestProcessingPlainCSV(t *testing.T) {
-	f, err := os.Open("testfiles/people.csv")
-
-	if err != nil {
-		t.Fatalf("test failed to open file; %s", err)
-	}
-
-	defer (func() {
-		if err := f.Close(); err != nil {
-			t.Fatalf("test failed to close file; %s", err)
-		}
-	})()
-
 	config := Config{Header: true}
 
-	processed, err := ForEach(f, config, func(*Row) error {
+	processed, err := ForEachFile("testfiles/people.csv", config, func(*Row) error {
 		return nil /* NOP */
 	})
 
