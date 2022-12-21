4a1fcce8f0b7adb4ec1afe33e6005347ec3d190d
Corey Prophitt <git@prophitt.me>
2022-12-21 13:02:33 -0800

    Basic tests added for processing

--- csv_test.go
+++ csv_test.go
@@ -1,1 +1,62 @@
 package csv
+
+import (
+	"os"
+	"testing"
+)
+
+func TestProcessingPlainGzippedCSV(t *testing.T) {
+	f, err := os.Open("testfiles/people.csv.gz")
+
+	if err != nil {
+		t.Fatalf("test failed to open file; %s", err)
+	}
+
+	defer (func() {
+		if err := f.Close(); err != nil {
+			t.Fatalf("test failed to close file; %s", err)
+		}
+	})()
+
+	config := Config{Header: true, Gzipped: true}
+
+	processed, err := ForEach(f, config, func(*Row) error {
+		return nil /* NOP */
+	})
+
+	if err != nil {
+		t.Fatalf("test failed process file; %s", err)
+	}
+
+	if processed != 4 {
+		t.Fatalf("test failed process correct row count; got %d", processed)
+	}
+}
+
+func TestProcessingPlainCSV(t *testing.T) {
+	f, err := os.Open("testfiles/people.csv")
+
+	if err != nil {
+		t.Fatalf("test failed to open file; %s", err)
+	}
+
+	defer (func() {
+		if err := f.Close(); err != nil {
+			t.Fatalf("test failed to close file; %s", err)
+		}
+	})()
+
+	config := Config{Header: true}
+
+	processed, err := ForEach(f, config, func(*Row) error {
+		return nil /* NOP */
+	})
+
+	if err != nil {
+		t.Fatalf("test failed process file; %s", err)
+	}
+
+	if processed != 4 {
+		t.Fatalf("test failed process correct row count; got %d", processed)
+	}
+}
