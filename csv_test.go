565320fce935a112df60c66df0b44d97b290a76a
Corey Prophitt <git@prophitt.me>
2022-12-23 08:08:51 -0800

    Added tsv tests

--- csv_test.go
+++ csv_test.go
@@ -68,3 +68,44 @@ func TestForEach(t *testing.T) {
 		}
 	}
 }
+
+func TestForEachWithTabs(t *testing.T) {
+	files := []string{
+		"testfiles/people.csv.gz",
+		"testfiles/people.csv",
+	}
+
+	valid := map[string]bool{
+		"jones": true,
+		"smith": true,
+	}
+
+	for _, f := range files {
+		reader, err := NewReaderFromFile(f)
+
+		if err != nil {
+			t.Fatalf("test failed open file; %s", err)
+		}
+
+		reader.Header = true
+		reader.Comma = '\t'
+
+		processed, err := reader.ForEach(func(row *Row) error {
+			if lname, ok := row.Get("last_name"); ok {
+				if _, ok := valid[lname]; !ok {
+					t.Fatalf("found invalid last name; %s", lname)
+				}
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			t.Fatalf("test failed to process reader; %s", err)
+		}
+
+		if processed != 4 {
+			t.Fatalf("test failed process correct row count; got %d", processed)
+		}
+	}
+}
